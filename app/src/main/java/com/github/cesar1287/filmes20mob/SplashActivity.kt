9210ef050c6a9443e0eaa8970b01d1ecd45120c9
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2021-03-14 20:39:33 -0300

    Added Splash screen

--- app/src/main/java/com/github/cesar1287/filmes20mob/SplashActivity.kt
+++ app/src/main/java/com/github/cesar1287/filmes20mob/SplashActivity.kt
@@ -1,11 +1,31 @@
 package com.github.cesar1287.filmes20mob
 
+import android.animation.Animator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.cesar1287.filmes20mob.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lavAnimationView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator?) { /** NOT USED*/ }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+
+            override fun onAnimationCancel(p0: Animator?) { /** NOT USED*/ }
+
+            override fun onAnimationRepeat(p0: Animator?) { /** NOT USED*/ }
+
+        })
     }
 }
