b8c37eee770d930e1886f48c605060f0f3230a7c
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2021-03-14 18:52:37 -0300

    Initial commit

---
+++ app/src/main/java/com/github/cesar1287/filmes20mob/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.cesar1287.filmes20mob
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
