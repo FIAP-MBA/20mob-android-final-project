e9e402d98732e062447bc7a8b873d727708136b7
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2021-03-20 12:29:00 -0300

    HomeFragment setup

--- app/src/main/java/com/github/cesar1287/filmes20mob/home/HomeFragment.kt
+++ app/src/main/java/com/github/cesar1287/filmes20mob/home/HomeFragment.kt
@@ -6,14 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.cesar1287.filmes20mob.R
+import com.github.cesar1287.filmes20mob.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var homeBinding: FragmentHomeBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        homeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return homeBinding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        homeBinding = null
     }
 }
