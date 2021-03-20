efa749d961c5e79625909a11dc44196322a2eb71
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2021-03-20 15:47:09 -0300

    added base files from di

--- app/src/main/java/com/github/cesar1287/filmes20mob/di/AppModule.kt
+++ app/src/main/java/com/github/cesar1287/filmes20mob/di/AppModule.kt
@@ -1,9 +1,12 @@
 package com.github.cesar1287.filmes20mob.di
 
+import com.github.cesar1287.filmes20mob.home.presentation.HomeViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 object AppModule {
-    val viewModelModules = module {
 
+    val viewModelModules = module {
+        viewModel { HomeViewModel(get()) }
     }
 }
--- app/src/main/java/com/github/cesar1287/filmes20mob/di/DataModule.kt
+++ app/src/main/java/com/github/cesar1287/filmes20mob/di/DataModule.kt
@@ -1,10 +1,19 @@
 package com.github.cesar1287.filmes20mob.di
 
+import com.github.cesar1287.filmes20mob.home.data.HomeRepository
+import com.github.cesar1287.filmes20mob.home.data.HomeRepositoryImpl
+import com.github.cesar1287.filmes20mob.home.data.HomeDataSourceFactory
+import com.github.cesar1287.filmes20mob.home.data.HomePageKeyedDataSource
 import org.koin.dsl.module
 
 object DataModule {
 
-    val repositoryModules = module {
+    val pagingModules = module {
+        single { HomePageKeyedDataSource(get(), get()) }
+        single { HomeDataSourceFactory(get()) }
+    }
 
+    val repositoryModules = module {
+        single<HomeRepository> { HomeRepositoryImpl() }
     }
 }
